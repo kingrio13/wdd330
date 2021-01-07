1490c0d22a591062d004ba24d33f774a6bff9274
kingrio13 <kingrio.miranda@gmail.com>
2021-01-07 21:24:54 +0800

    first week

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
+
+
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week 1 notes",
       url: "week1/index.html"
+    },
+    {
+      label: "Week 2 notes",
+      url: "week2/index.html"
+    },
+    {
+      label: "Challenge 1",
+      url: "challenge"
     }
   ]
 
-  alert('wew');
+
+
+let ai;
+for (ai=0; ai<links.length; ai++){
+  let li= document.createElement('li');
+  let ali=document.createElement('a');
+ali.textContent=links[ai].label;
+ali.setAttribute('href', links[ai].url);
+li.appendChild(ali);
+
+console.log(li)
+document.querySelector('#tablecontent').appendChild(li);
+}
