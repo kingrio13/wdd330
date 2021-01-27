3d6ebf76fbd9f30dd2d4e66fc116d0305f3ddb39
kingrio13 <kingrio.miranda@gmail.com>
2021-01-28 05:53:45 +0800

    Week 4 partial

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
 
 
-const links = [
-    {
-      label: "Week 1 notes",
-      url: "week1/index.html"
-    },
-    {
-      label: "Week 2 Notes",
-      url: "week2/index.html"
-    },
-    {
-      label: "Week 3 Notes",
-      url: "week3/index.html"
-    }
-  ]
-
-
-
-let ai;
-for (ai=0; ai<links.length; ai++){
-  let li= document.createElement('li');
-  let ali=document.createElement('a');
-ali.textContent=links[ai].label;
-ali.setAttribute('href', links[ai].url);
-li.appendChild(ali);
-
-console.log(li)
-document.querySelector('#tablecontent').appendChild(li);
-}
-
-
 document.getElementById("currentYear").innerHTML=new Date().getFullYear();
---
+++ js/tc.js
@@ -0,0 +1,33 @@
+
+
+const links = [
+    {
+      label: "Week 1 notes",
+      url: "week1/index.html"
+    },
+    {
+      label: "Week 2 Notes",
+      url: "week2/index.html"
+    },
+    {
+      label: "Week 3 Notes",
+      url: "week3/index.html"
+    },
+    {
+        label: "Week 4 Notes",
+        url: "week4/index.html"
+      }
+  ]
+
+
+
+let ai;
+for (ai=0; ai<links.length; ai++){
+  let li= document.createElement('li');
+  let ali=document.createElement('a');
+ali.textContent=links[ai].label;
+ali.setAttribute('href', links[ai].url);
+li.appendChild(ali);
+
+document.querySelector('#tablecontent').appendChild(li);
+}
